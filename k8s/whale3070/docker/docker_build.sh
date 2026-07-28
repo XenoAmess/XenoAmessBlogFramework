2bd2c2065e4215a06723e7bedfd2114a8f0694d0
XenoAmess <xenoamess@gmail.com>
2026-07-28 11:35:50 +0800

    update

--- k8s/whale3070/docker/docker_build.sh
+++ k8s/whale3070/docker/docker_build.sh
@@ -1,5 +1,5 @@
 export tag=$(date "+%Y%m%d_%H%M%S")
-docker build -t 23.94.174.102:8911/xenoamess/whale3070blog:${tag} -f Dockerfile ../ --build-arg CACHEBUST=${tag}
-docker tag 23.94.174.102:8911/xenoamess/whale3070blog:${tag} 23.94.174.102:8911/xenoamess/whale3070blog:latest
-docker push 23.94.174.102:8911/xenoamess/whale3070blog:${tag}
-docker push 23.94.174.102:8911/xenoamess/whale3070blog:latest
+docker build -t xenoamess/whale3070blog:${tag} -f Dockerfile ../ --build-arg CACHEBUST=${tag}
+docker tag xenoamess/whale3070blog:${tag} xenoamess/whale3070blog:latest
+docker push xenoamess/whale3070blog:${tag}
+docker push xenoamess/whale3070blog:latest
