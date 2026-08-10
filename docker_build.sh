b910591e5af5f6cd2988070afafc467712177de6
XenoAmess <xenoamess@gmail.com>
2026-08-10 11:58:51 +0800

    build: route image build traffic through host proxy; drop unused whale3070 submodule

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,16 @@
 docker pull node:26
 export tag=$(date "+%Y%m%d_%H%M%S")
-docker build -t xenoamess/xablog:${tag} . --build-arg CACHEBUST=${tag}
+HOST_IP=$(ip -4 addr show docker0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+HOST_IP=${HOST_IP:-host.docker.internal}
+export PROXY="http://${HOST_IP}:10808"
+docker build \
+  --build-arg HTTP_PROXY=$PROXY \
+  --build-arg HTTPS_PROXY=$PROXY \
+  --build-arg http_proxy=$PROXY \
+  --build-arg https_proxy=$PROXY \
+  --build-arg ALL_PROXY=$PROXY \
+  --build-arg CACHEBUST=${tag} \
+  -t xenoamess/xablog:${tag} .
 docker push xenoamess/xablog:${tag}
 docker tag xenoamess/xablog:${tag} xenoamess/xablog:latest
 docker push xenoamess/xablog:latest
--- k8s/whale3070/docker/docker_build.sh
+++ k8s/whale3070/docker/docker_build.sh
@@ -1,6 +1,16 @@
 docker pull node:26
 export tag=$(date "+%Y%m%d_%H%M%S")
-docker build -t xenoamess/whale3070blog:${tag} -f Dockerfile ../ --build-arg CACHEBUST=${tag}
+HOST_IP=$(ip -4 addr show docker0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+HOST_IP=${HOST_IP:-host.docker.internal}
+export PROXY="http://${HOST_IP}:10808"
+docker build \
+  --build-arg HTTP_PROXY=$PROXY \
+  --build-arg HTTPS_PROXY=$PROXY \
+  --build-arg http_proxy=$PROXY \
+  --build-arg https_proxy=$PROXY \
+  --build-arg ALL_PROXY=$PROXY \
+  --build-arg CACHEBUST=${tag} \
+  -t xenoamess/whale3070blog:${tag} -f Dockerfile ../
 docker tag xenoamess/whale3070blog:${tag} xenoamess/whale3070blog:latest
 docker push xenoamess/whale3070blog:${tag}
 docker push xenoamess/whale3070blog:latest
